f2f7f330bcfb051edc0498efaa52c14a08e5eb51
baixiaoji <1041874421@qq.com>
2018-10-12 23:18:04 +0800

    [add] radarMap

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/index'
+import radarMap from '@/components/radarMap'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'index',
       component: Index
+    },
+    {
+      path: '/radarMap',
+      name: 'radarMap',
+      component: radarMap
     }
   ]
 })
