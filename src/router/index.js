ba6d01cf8fb8f7cd7d937e11f3516b9fc340c167
zoulijie <zoulijie@u51.com>
2018-11-22 08:18:36 +0800

    build

--- src/router/index.js
+++ src/router/index.js
@@ -2,20 +2,21 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/index'
 import radarMap from '@/components/radarMap'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'index',
-    //   component: Index
-    // },
-    // {
-    //   path: '/radarMap',
-    //   name: 'radarMap',
-    //   component: radarMap
-    // }
+    {
+      path: '/',
+      name: 'index',
+      component: HelloWorld
+    },
+    {
+      path: '/radarMap',
+      name: 'radarMap',
+      component: HelloWorld
+    }
   ]
 })
